6e0de28e623dd842b4bc4c9b8dca655ff0258d57
artyomvorobiov <89642304+artyomvorobiov@users.noreply.github.com>
2025-03-25 19:56:10 +0300

    Update photo_service.dart

--- services/photo_service.dart
+++ services/photo_service.dart
@@ -19,11 +19,11 @@ class PhotoService {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   List<Map<String, dynamic>> _serverPhotos = [];
   final minio = Minio(
-    endPoint: '91.197.98.163',
-    port: 9000,
-    accessKey: 'minioadmin',
-    secretKey: 'minioadminpassword',
-    useSSL: false,
+    endPoint: '****',
+    port: '****',
+    accessKey: '****',
+    secretKey: '****',
+    useSSL: '****',
   );
 
   Future<String> uploadPhotoToServer(AssetEntity localPhoto) async {
