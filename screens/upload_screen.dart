5873f28005673f857724b8e6248f20ff7d522f09
artyomvorobiov <89642304+artyomvorobiov@users.noreply.github.com>
2025-04-22 15:39:48 +0300

    Update upload_screen.dart

--- screens/upload_screen.dart
+++ screens/upload_screen.dart
@@ -5,6 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:trip/services/photo_service.dart';
 
+const Color kBackgroundColor = Color(0xFFF5EEDC);
+const Color kPrimaryColor = Color(0xFF27548A); 
+const Color kAppBarColor = Color(0xFF183B4E);
+const Color kAccentColor = Color(0xFFDDA853); 
+
 class UploadScreen extends StatefulWidget {
   @override
   _UploadScreenState createState() => _UploadScreenState();
@@ -13,7 +18,6 @@ class UploadScreen extends StatefulWidget {
 class _UploadScreenState extends State<UploadScreen>
     with SingleTickerProviderStateMixin {
   final ImagePicker _picker = ImagePicker();
-
   File? _selectedImage;
   final PhotoService _photoService = PhotoService();
   late AnimationController _loadingController;
@@ -21,9 +25,10 @@ class _UploadScreenState extends State<UploadScreen>
   @override
   void initState() {
     super.initState();
-    _loadingController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2))
-          ..repeat();
+    _loadingController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat();
   }
 
   @override
@@ -47,7 +52,6 @@ class _UploadScreenState extends State<UploadScreen>
           icon: Icons.error_outline, backgroundColor: Colors.redAccent);
       return;
     }
-
     try {
       User? currentUser = FirebaseAuth.instance.currentUser;
       if (currentUser == null) {
@@ -55,17 +59,15 @@ class _UploadScreenState extends State<UploadScreen>
             icon: Icons.error_outline, backgroundColor: Colors.redAccent);
         return;
       }
-
       showDialog(
         context: context,
         barrierDismissible: false,
         builder: (context) =>
             buildCustomLoadingOverlay('Загрузка фото на сервер...'),
       );
-
-      bool uploaded = await _photoService.uploadImage(_selectedImage, currentUser);
-      Navigator.pop(context);
-
+      bool uploaded =
+          await _photoService.uploadImage(_selectedImage, currentUser);
+      Navigator.pop(context); 
       if (!uploaded) {
         _showCustomMessage('Фото уже сохранено на сервере.',
             icon: Icons.info_outline, backgroundColor: Colors.orange);
@@ -82,52 +84,56 @@ class _UploadScreenState extends State<UploadScreen>
           icon: Icons.error_outline, backgroundColor: Colors.redAccent);
     }
   }
+
   Widget buildCustomLoadingOverlay(String message) {
     return Container(
-    color: Colors.black.withOpacity(0.6),
-    child: Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          AnimatedBuilder(
-            animation: _loadingController,
-            builder: (context, child) {
-              return Transform.rotate(
-                angle: _loadingController.value * 6.28,
-                child: child,
-              );
-            },
-            child: ClipOval( 
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0xFFFC5C7D), Color(0xFF6A82FB)],
+      color: Colors.black.withOpacity(0.6),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AnimatedBuilder(
+              animation: _loadingController,
+              builder: (context, child) {
+                return Transform.rotate(
+                  angle: _loadingController.value * 6.28,
+                  child: child,
+                );
+              },
+              child: ClipOval(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [kPrimaryColor, kAccentColor],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
                   ),
-                ),
-                child: const Icon(
-                  Icons.photo,
-                  size: 50,
-                  color: Colors.white,
+                  child: const Icon(
+                    Icons.photo,
+                    size: 50,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-           Text(
-           message,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.none,
-            ),
-          )
-        ],
+            const SizedBox(height: 20),
+            Text(
+              message,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.none,
+              ),
+            )
+          ],
+        ),
       ),
-    ),
-  );
+    );
   }
+
   void _showCustomMessage(String message,
       {IconData icon = Icons.check_circle_outline,
       Color backgroundColor = Colors.green}) {
@@ -139,13 +145,41 @@ class _UploadScreenState extends State<UploadScreen>
             Icon(icon, color: Colors.white),
             const SizedBox(width: 8),
             Expanded(
-                child: Text(message, style: const TextStyle(color: Colors.white))),
+                child:
+                    Text(message, style: const TextStyle(color: Colors.white))),
           ],
         ),
         behavior: SnackBarBehavior.floating,
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         duration: const Duration(seconds: 3),
+      ),
+    );
+  }
+
+  Widget _buildCustomAppBar() {
+    return Container(
+      height: 60,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: kAppBarColor.withOpacity(0.9),
+      ),
+      child: Row(
+        children: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back, color: kBackgroundColor),
+            onPressed: () => Navigator.pop(context),
+          ),
+          const SizedBox(width: 8),
+          const Text(
+            'Загрузка фотографии',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: kBackgroundColor,
+            ),
+          ),
+          const Spacer(),
+        ],
       ),
     );
   }
@@ -154,9 +188,12 @@ class _UploadScreenState extends State<UploadScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFF89CFFD), Color(0xFFB084CC)],
+            colors: [
+              kBackgroundColor,
+              kPrimaryColor.withOpacity(0.1),
+            ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           ),
@@ -168,32 +205,36 @@ class _UploadScreenState extends State<UploadScreen>
               Expanded(
                 child: Center(
                   child: Card(
-                    color: Colors.white.withOpacity(0.85),
+                    color: kBackgroundColor.withOpacity(0.95),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(24),
                     ),
-                    elevation: 4,
+                    elevation: 6,
                     margin: const EdgeInsets.symmetric(horizontal: 24),
                     child: Padding(
                       padding: const EdgeInsets.all(24.0),
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          const Text(
+                          Text(
                             'Загрузка фото',
                             style: TextStyle(
                               fontSize: 22,
                               fontWeight: FontWeight.bold,
+                              color: kPrimaryColor,
                             ),
                           ),
                           const SizedBox(height: 16),
                           if (_selectedImage != null)
                             Column(
                               children: [
-                                Image.file(
-                                  _selectedImage!,
-                                  height: 200,
-                                  fit: BoxFit.cover,
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(12),
+                                  child: Image.file(
+                                    _selectedImage!,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                                 const SizedBox(height: 16),
                               ],
@@ -201,24 +242,23 @@ class _UploadScreenState extends State<UploadScreen>
                           ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               minimumSize: const Size(double.infinity, 50),
+                              backgroundColor: kAccentColor,
                               shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
+                                  borderRadius: BorderRadius.circular(12)),
                             ),
                             onPressed: _selectImage,
-                            child: const Text('Выбрать фото'),
+                            child: const Text('Выбрать фото', style: TextStyle(color: kPrimaryColor),),
                           ),
                           const SizedBox(height: 16),
                           ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               minimumSize: const Size(double.infinity, 50),
-                              backgroundColor: Colors.blue,
+                              backgroundColor: kPrimaryColor,
                               shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
+                                  borderRadius: BorderRadius.circular(12)),
                             ),
                             onPressed: _uploadImage,
-                            child: const Text('Загрузить фото'),
+                            child: const Text('Загрузить фото', style: TextStyle(color: kBackgroundColor),),
                           ),
                         ],
                       ),
@@ -229,32 +269,6 @@ class _UploadScreenState extends State<UploadScreen>
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _buildCustomAppBar() {
-    return Container(
-      height: 60,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      color: Colors.black.withOpacity(0.1),
-      child: Row(
-        children: [
-          IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => Navigator.pop(context),
-          ),
-          const SizedBox(width: 8),
-          const Text(
-            'Загрузка фотографии',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          const Spacer(),
-        ],
       ),
     );
   }
